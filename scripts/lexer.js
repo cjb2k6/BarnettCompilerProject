964ac95b0c85056afc0f0f7cfbec785de7a835ac
cjb2k6 <cjb2k6@gmail.com>
2015-02-18 14:51:53 -0500

    Basic Lex Test Conditions
    Implemented the beginning of the lexer's conditions when certain
    characters come up. Also tested the functionality of the token object.

--- scripts/lexer.js
+++ scripts/lexer.js
@@ -1,6 +1,12 @@
+//Global Variables
+var lineNum = 1; //Keep track of the line number
+
 //FUNCTIONS---------------------------------------------------------------------------
 //This is the main function of the lexer.
 function lex(){
+	//Initialize
+	lineNum = 1;
+	
 	// Get the source code from the text area.
     var sourceCode = document.getElementById("taSourceCode").value;
 	
@@ -12,14 +18,45 @@ function lex(){
 	
 	//Process each line of input in the source code
 	for(var i = 0; i < lineStrings.length; i++){
-			processLine(i);
+			processLine(lineStrings[i]);
+			lineNum++;
 	}
 }
 //Processes each line of the source code into tokens
-function processLine(index){
-	//Split the line into an array of characters
-	charArray = lineStrings[index].split("");
-	//More Stuff
+function processLine(line){
+	var isString = false;
+	
+	for(var j = 0; j < line.length; j++){
+		//Test for space
+		if(line[j].match(/\s/)){
+			//Is the space in a string?
+			if(isString){
+				//Create the space token, add to tokens
+				tokens[tokenIndex] = new tokenObj(lineNum, "T_SPACE", " ");
+				tokenIndex++;
+			}
+			//Else, do nothing, just skip the space
+			
+		//Test for character a-z
+		}else if(line[j].match(/[a-z]/)){
+			//If its in a string, then its a char
+			if(isString){
+				tokens[tokenIndex] = new tokenObj(lineNum, "T_CHAR", line[j]);
+				tokenIndex++;
+				
+			//If not in a string, it could be an identifier or part of a keyword
+			} else {
+				tokens[tokenIndex] = new tokenObj(lineNum, "T_ID", line[j]);
+				tokenIndex++;
+			}
+			
+		
+		}else{
+		 tokens[tokenIndex] = "No Match.";
+		 tokenIndex++;
+		}
+	}
+	
 }
 
 //Splits the source code up by line
@@ -35,8 +72,11 @@ function splitByLine(src){
 		   type:String  - the type of token it is
 		   value:String - the actual token itself
    -------- */
-function tokenObj(lineNum, type, value){
-	this.lineNum = lineNum;
+function tokenObj(lineNumber, type, value){
+	this.lineNumber = lineNumber;
 	this.type = type;
 	this.value = value;
+	this.toString = function(){
+		return "" + this.lineNumber + " " + this.type + " \"" + this.value + "\"";
+	};
 }
