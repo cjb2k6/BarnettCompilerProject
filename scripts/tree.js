460703cb8b63751fb6a125190930fb61f66a913d
cjb2k6 <cjb2k6@gmail.com>
2015-04-08 12:47:45 -0400

    Added Tree toString
    Added the tree toString from the jsTreeDemo for prinitng purposes.

--- scripts/tree.js
+++ scripts/tree.js
@@ -49,4 +49,44 @@ function Tree(){
 		this.current = current.parent;
 	};
 	
+	//Function from jsTreeDemo to print the tree.
+	// Return a string representation of the tree.
+    this.toString = function() {
+        // Initialize the result string.
+        var traversalResult = "";
+
+        // Recursive function to handle the expansion of the nodes.
+        function expand(node, depth)
+        {
+            // Space out based on the current depth so
+            // this looks at least a little tree-like.
+            for (var i = 0; i < depth; i++)
+            {
+                traversalResult += "-";
+            }
+
+            // If there are no children (i.e., leaf nodes)...
+            if (!node.children || node.children.length === 0)
+            {
+                // ... note the leaf node.
+                traversalResult += "[" + node.name + "]";
+                traversalResult += "\n";
+            }
+            else
+            {
+                // There are children, so note these interior/branch nodes and ...
+                traversalResult += "<" + node.name + "> \n";
+                // .. recursively expand them.
+                for (var i = 0; i < node.children.length; i++)
+                {
+                    expand(node.children[i], depth + 1);
+                }
+            }
+        }
+        // Make the initial call to expand from the root.
+        expand(this.root, 0);
+        // Return the result.
+        return traversalResult;
+    };
+	
 }
